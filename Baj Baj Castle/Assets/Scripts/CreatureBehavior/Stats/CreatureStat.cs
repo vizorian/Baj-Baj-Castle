c431f305096812472e1615aa6c8ac054e8fbcad0
vizorian <birstonasvilius@gmail.com>
2023-09-12 21:55:35 +0300

    Added creature stat & stat modifier classes

--- Baj Baj Castle/Assets/Scripts/CreatureBehavior/Stats/CreatureStat.cs
+++ Baj Baj Castle/Assets/Scripts/CreatureBehavior/Stats/CreatureStat.cs
@@ -1,17 +1,71 @@
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
 
 namespace CreatureBehavior.Stats
 {
     public class CreatureStat
     {
-        public int BaseValue { get; set; }
-
+        public int BaseValue { get; }
         private readonly List<StatModifier> _statModifiers;
+
+        public int Value
+        {
+            get
+            {
+                if (_isDirty)
+                {
+                    _value = CalculateFinalValue();
+                    _isDirty = false;
+                }
+
+                return _value;
+            }
+        }
+
+        private int _value;
+        private bool _isDirty = true;
 
         public CreatureStat(int baseValue)
         {
             BaseValue = baseValue;
+            _value = CalculateFinalValue();
             _statModifiers = new List<StatModifier>();
+        }
+
+        public void AddModifier(StatModifier modifier)
+        {
+            _statModifiers.Add(modifier);
+            _isDirty = true;
+        }
+
+        public void RemoveModifier(StatModifier modifier)
+        {
+            _statModifiers.Remove(modifier);
+            _isDirty = true;
+        }
+
+        // TODO: modifier calculation logic
+        private int CalculateFinalValue()
+        {
+            float finalValue = BaseValue;
+
+            foreach (var statModifier in _statModifiers)
+            {
+                switch (statModifier.Type)
+                {
+                    case StatModifierType.Flat:
+                        break;
+                    case StatModifierType.Percentage:
+                        break;
+                    default:
+                        throw new InvalidEnumArgumentException();
+                }
+                finalValue += statModifier.Value;
+            }
+
+            return (int)Math.Round(finalValue);
         }
     }
 }
--- Baj Baj Castle/Assets/Scripts/CreatureBehavior/Stats/StatModifier.cs
+++ Baj Baj Castle/Assets/Scripts/CreatureBehavior/Stats/StatModifier.cs
@@ -1,12 +1,20 @@
 namespace CreatureBehavior.Stats
 {
+    public enum StatModifierType
+    {
+        Flat,
+        Percentage
+    }
+    
     public class StatModifier
     {
         public readonly float Value;
+        public readonly StatModifierType Type;
 
-        public StatModifier(float value)
+        public StatModifier(float value, StatModifierType type)
         {
             Value = value;
+            Type = type;
         }
     }
 }
